fb5e3a384309de150dc0db7421822eb58f0a4515
OmairaBarrera <lomaira.barrera@gmail.com>
2023-02-13 06:55:44 -0500

    Solucion ejercicio 2

---
+++ ejercicio2.py
@@ -0,0 +1,5 @@
+""" 2. En la jerarquía de operadores, cuáles son los que más
+prioridad tienen cuando el intérprete de Python los evalúa? """
+
+print(f'En la jerarquia de operadores, los de mayor priodidad son las operaiones entre paréntesis')
+print(f'(3*45-34)+(3-5) = ({3*45-34})+({3-5}) = {(3*45-34)+(3-5)}')
