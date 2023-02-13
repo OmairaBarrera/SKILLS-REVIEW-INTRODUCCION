339abc8d60167584f9aa4af043771aa20aaba600
OmairaBarrera <lomaira.barrera@gmail.com>
2023-02-13 07:01:21 -0500

    Solucion ejercicio 3

---
+++ ejercicio3.py
@@ -0,0 +1,9 @@
+""" 3. Si hay operadores de igual precedencia, en qué orden se
+ejecutan?
+
+A. De izquierda a derecha
+B. De derecha a izquierda """
+
+print(f'Se ejecutan: A. De izquierda a derecha')
+print(f'2*8*6*9 = {2*8}*6*9 = {2*8*6}*9 = {2*8*6*9}')
+print(f'2+6+9-5 = {2+6}+9-5 = {2+6+9}-5 = {2+6+9-5}')
