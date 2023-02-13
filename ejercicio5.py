530211d449ccada840a5c5aa3b94b3b3ed848d5a
OmairaBarrera <lomaira.barrera@gmail.com>
2023-02-13 07:13:24 -0500

    Solucion ejercicio 5

---
+++ ejercicio5.py
@@ -0,0 +1,14 @@
+""" 5. Enumere 5 tipos de datos en Python y suministre un valor de
+ejemplo de cada uno. """
+
+nombre="Omaira"
+edad=23
+estatura=1.58
+estudiante=True
+gustos=["Danza","Ciclismo"]
+print(f'tipos de datos')
+print(f'1. Cadena = Nombre:{nombre}')
+print(f'2. Numero entero = Edad:{edad}')
+print(f'3. Numero decimal = Estatura:{estatura}')
+print(f'4. Booleano = Eres estudiante:{estudiante}')
+print(f'5. Secuencia = Tus gustos son: 1.{gustos[0]}, 2.{gustos[1]}')
