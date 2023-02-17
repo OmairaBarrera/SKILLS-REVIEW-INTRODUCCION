c56a84d18ab8441e40105a8e969a434f287924de
Omaira <lomaira.barrera@gmail.com>
2023-02-16 22:20:49 -0500

    Solucion ejercicio 4

--- ejercicio4.py
+++ ejercicio4.py
@@ -1,1 +1,10 @@
 """ 4. Que son las expresiones regulares en Python? """
+import re
+
+print(f'Expresiones regulares')
+print(f'se puede considerar un mini leguaje de programacion dentro de otro, son secuencias de caracteres que forman un patron de busqueda')
+print(f'Expresion regular para hacer una busqueda y localizarlo donde coincida')
+cadena="Investigacion de expresiones regulares"
+print(re.search("regulares", cadena))
+
+
