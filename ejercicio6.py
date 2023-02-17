5d51542f5bc0d8225a16d3e8d7107d4c4900b24b
Omaira <lomaira.barrera@gmail.com>
2023-02-16 21:44:00 -0500

    Solucion Ejercicio 6

--- ejercicio6.py
+++ ejercicio6.py
@@ -1,2 +1,10 @@
 """ En sus propias palabras, qué son las funciones
 preconstruidas y proporcione 2 ejemplos. """
+
+print(f'Funciones preconstruidas o predefinidas')
+print(f'Son aquellas que nos proporciona con el lenguaje de programacion')
+print(f'Ejemplo 1: la funcion len determina la longitud de una cadena')
+print(f'La palabra MARGARITA tiene una longitud: {len("margarita")}')
+print(f'Ejempño 2: la funcion max determina el maximo de grupo de numeros')
+x=[3,5,7,8,2,8]
+print(f'El numero mayor de los siguientes numeros {x} es: {max(x)}')
